f8a9f7b261ed68112a5c08f539e7183efa3f0ea0
Danny Evins <dcastkid@gmail.com>
2016-07-20 12:18:09 -0500

    form works now

--- app/controllers/alien-new.js
+++ app/controllers/alien-new.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  randomString: 'string',
+  actions: {
+    newSiting() {
+      const data ={
+        city: this.city,
+        state: this.state,
+        rating: this.rating,
+      };
+
+      fetch('http://tiny-tn.herokuapp.com/collections/aliens', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify(data),
+      });
+    }
+  }
 });
