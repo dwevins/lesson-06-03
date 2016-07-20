4d6ecbbce987ae8cc2104640f0ac64a92aed52bd
Danny Evins <dcastkid@gmail.com>
2016-07-20 11:35:43 -0500

    many changes

--- app/routes/near-earth.js
+++ app/routes/near-earth.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+import config from '../config/environment'
 
 export default Ember.Route.extend({
+model({ page }) {
+    // const page = params.page
+    // const { page } = params;
+
+    return fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${config.nasaKey}&page=${page}`)
+      .then((res) => res.json());
+  }
 });
