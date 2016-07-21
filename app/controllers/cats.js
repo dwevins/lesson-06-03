b77490b8f98ab446597b177743b25d4b935f4fdd
Danny Evins <dcastkid@gmail.com>
2016-07-21 12:11:42 -0500

    stuff and things

--- app/controllers/cats.js
+++ app/controllers/cats.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    deleteCat(cat) {
+      if (confirm('Adopt this cat?')) {
+        fetch('https://tiny-tn.herokuapp.com/collections/dwe-cats/' + cat._id, {
+          method: 'delete',
+        })
+        .then(() => {
+          this.set('model', this.model.filter((item) => {
+            return item._id !== cat._id;
+          }))
+        })
+      }
+    },
+
     addCat() {
       const newObj = {
         name: this.name,
@@ -13,15 +26,19 @@ export default Ember.Controller.extend({
       if (newObj.name === '' || newObj.color === '') {
         alert('Cats must have name and color');
         return;
-      }
+      };
 
       fetch('https://tiny-tn.herokuapp.com/collections/dwe-cats', {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
         method: 'post',
         body: JSON.stringify(newObj),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res.json())
+      .then((cat) => {
+        this.set('model', [...this.model, cat]);
       });
     }
   }
--- app/routes/cats.js
+++ app/routes/cats.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  model() {
+    return fetch('https://tiny-tn.herokuapp.com/collections/dwe-cats')
+      .then((res) => res.json());
+  }
 });
